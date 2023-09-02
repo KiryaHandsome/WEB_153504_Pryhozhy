82c5db214cfa554e6b6bf5db7fee03d04221765a
kiril <kiril@DESKTOP-7L689H5>
2023-09-02 17:32:02 +0300

    Add project files.

---
+++ WEB_153504_Pryhozhy/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153504_Pryhozhy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, world!";
+        }
+    }
+}
