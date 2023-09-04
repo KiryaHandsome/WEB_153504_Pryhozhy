3dc6806100e65c56adc0e6c22705f43de6316d36
KiryaHandsome <kirill19962003@gmail.com>
2023-09-04 13:20:30 +0300

    Add partial views

--- WEB_153504_Pryhozhy/Controllers/HomeController.cs
+++ WEB_153504_Pryhozhy/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WEB_153504_Pryhozhy.Models;
 
 namespace WEB_153504_Pryhozhy.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Index()
         {
-            return View();
+            List<ListItemModel> demoList = new List<ListItemModel>
+            {
+                new ListItemModel { Id = 1, Name = "Item1" },
+                new ListItemModel { Id = 2, Name = "Item2" },
+                new ListItemModel { Id = 3, Name = "Item3" }
+            };
+
+            SelectList selectList = new SelectList(demoList, "Id", "Name");
+
+            return View(selectList);
         }
     }
 }
---
+++ WEB_153504_Pryhozhy/Models/ListItemModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WEB_153504_Pryhozhy.Models
+{
+    public class ListItemModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+    }
+}
