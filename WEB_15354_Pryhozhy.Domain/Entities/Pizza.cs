0962351d48b90cf41fb65a7df40c447d19c24738
KiryaHandsome <kirill19962003@gmail.com>
2023-09-04 13:44:41 +0300

    Add domain project and entities

---
+++ WEB_15354_Pryhozhy.Domain/Entities/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace WEB_15354_Pryhozhy.Domain.Entities
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Calories { get; set; }
+        public string? Image {  get; set; }
+        public int CategoryId { get; set; }
+        public string? Description { get; set; }
+    }
+}
