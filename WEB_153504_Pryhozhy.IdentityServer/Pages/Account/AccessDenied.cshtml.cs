415cd4aef04234528c82bc2ff3b863db12052792
KiryaHandsome <kirill19962003@gmail.com>
2023-09-18 15:33:04 +0300

    feat: add identity server to project

---
+++ WEB_153504_Pryhozhy.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEB_153504_Pryhozhy.IdentityServer.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ WEB_153504_Pryhozhy.IdentityServer/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace WEB_153504_Pryhozhy.IdentityServer.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
