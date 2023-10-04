e5a94bb55c462e2c8fb1c6302bf9fb2439d8e687
KiryaHandsome <kirill19962003@gmail.com>
2023-10-04 11:17:44 +0300

    fix tag helper

--- WEB_153504_Pryhozhy/TagHelpers/PagerTagHelper.cs
+++ WEB_153504_Pryhozhy/TagHelpers/PagerTagHelper.cs
@@ -1,54 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using static System.Net.Mime.MediaTypeNames;
 
-[HtmlTargetElement("Pager")]
-public class PagerTagHelper : TagHelper
+namespace WEB_153504_Pryhozhy.TagHelpers
 {
-    // Properties to configure the pagination
-    public int CurrentPage { get; set; }
-    public int TotalPages { get; set; }
-
-    public override void Process(TagHelperContext context, TagHelperOutput output)
+    [HtmlTargetElement("Pager", Attributes = "current-page, total-pages")]
+    public class PagerTagHelper : TagHelper
     {
-        // Create a div element with the specified class
-        var divTag = new TagBuilder("div");
-        divTag.AddCssClass("pagination justify-content-center");
+        // Properties to configure the pagination
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
 
-        // Add the previous page link
-        divTag.InnerHtml.AppendHtml(CreatePageLink("&laquo;", CurrentPage - 1, CurrentPage == 1));
-
-        // Add page links for each page number
-        for (var pageNum = 1; pageNum <= TotalPages; pageNum++)
+        public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            divTag.InnerHtml.AppendHtml(CreatePageLink(pageNum.ToString(), pageNum, pageNum == CurrentPage));
+            // Create a div element with the specified class
+            var divTag = new TagBuilder("div");
+            divTag.AddCssClass("pagination justify-content-center");
+
+            // Add the previous page link
+            divTag.InnerHtml.AppendHtml(CreateArrowLink("&laquo;", CurrentPage - 1));
+
+            for (var pageNum = 1; pageNum <= TotalPages; pageNum++)
+            {
+                divTag.InnerHtml.AppendHtml(CreatePageLink(pageNum.ToString(), pageNum));
+            }
+
+            // Add the next page link
+            divTag.InnerHtml.AppendHtml(CreateArrowLink("&raquo;", CurrentPage + 1));
+
+            // Set the result as the inner HTML of the custom-pagination tag
+            output.TagName = "ul";
+            output.Content.SetHtmlContent(divTag);
         }
 
-        // Add the next page link
-        divTag.InnerHtml.AppendHtml(CreatePageLink("&raquo;", CurrentPage + 1, CurrentPage == TotalPages));
+        private TagBuilder CreateArrowLink(string symbol, int targetPage)
+        {
+            var liTag = new TagBuilder("li");
+            liTag.AddCssClass("page-item");
+            if (targetPage < 1 || targetPage > TotalPages)
+            {
+                liTag.AddCssClass("disabled");
+            }
+            else
+            {
+                liTag.AddCssClass("active");
+            }
 
-        // Set the result as the inner HTML of the custom-pagination tag
-        output.TagName = "ul";
-        output.Content.SetHtmlContent(divTag);
+            var linkTag = new TagBuilder("a");
+            linkTag.AddCssClass("page-link");
+            linkTag.Attributes.Add("href", $"/catalog?pageNo={targetPage}");
+            linkTag.InnerHtml.AppendHtml(symbol);
+            liTag.InnerHtml.AppendHtml(linkTag);
+
+            return liTag;
+        }
+
+        private TagBuilder CreatePageLink(string text, int pageNo)
+        {
+            var liTag = new TagBuilder("li");
+            liTag.AddCssClass("page-item");
+            liTag.AddCssClass(CurrentPage == pageNo ? "active" : "");
+
+            var aTag = new TagBuilder("a");
+            aTag.AddCssClass("page-link");
+
+            aTag.Attributes.Add("href", $"/catalog?pageNo={pageNo}");
+            aTag.InnerHtml.AppendHtml(text);
+
+            liTag.InnerHtml.AppendHtml(aTag);
+
+            return liTag;
+        }
     }
 
-    private TagBuilder CreatePageLink(string text, int pageNo, bool isDisabled)
-    {
-        var liTag = new TagBuilder("li");
-        liTag.AddCssClass("page-item");
-        liTag.AddCssClass(isDisabled ? "disabled" : "");
-
-        var aTag = new TagBuilder("a");
-        aTag.AddCssClass("page-link");
-
-        // Hardcode the controller name here (e.g., "pizza")
-        var controller = "pizza";
-
-        // Generate the URL with the hardcoded controller
-        aTag.Attributes.Add("href", $"/{controller}?pageNo={pageNo}");
-        aTag.InnerHtml.AppendHtml(text);
-
-        liTag.InnerHtml.AppendHtml(aTag);
-
-        return liTag;
-    }
 }
